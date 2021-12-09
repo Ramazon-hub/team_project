ceff72b3eb51a9718895649418251de94d88c5f3
MahdiyDev <mahdiydev@gmail.com>
2021-12-09 22:57:15 +0500

    header main added

---
+++ front/src/Components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import './Main.scss'
+
+function Main({ children }) {
+    return (
+        <main>
+            <div className="main_warpper">
+
+            </div>
+            {children}
+            <div className="main_warpper">
+                
+            </div>
+        </main>
+    )
+}
+
+export default Main;
