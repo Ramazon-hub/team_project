5be7733593dbe3d0a5ec282d6dd6c6a80d3d151d
Ramazon-hub <ramazonpardayev@gmail.com>
2021-12-09 23:36:37 +0500

    puling Home

--- back/lib/postgres.js
+++ back/lib/postgres.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const { pg } = require("../config");
-const pool = new Pool({ connectionString: pg.connectionElephantString });
+const pool = new Pool({ connectionString: pg.connectionString });
 
 const fetch = async (SQL, ...values) => {
   const client = await pool.connect();
